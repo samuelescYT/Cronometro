e78322e62dcdcd176e041fad41c6b6ba83406b91
Samuel Campos <menudesamy@gmail.com>
2021-11-30 12:03:33 -0500

    se cambio la posicion del titulo y se solucionaron errores

--- main.js
+++ main.js
@@ -6,8 +6,9 @@ let intervalo;
 document.querySelector(".texto").innerHTML = horas + " horas : " + minutos + " minutos : " + segundos + " segundos";
 
 function iniciar() {
+    document.querySelector(".iniciar").removeAttribute("onclick");
     document.querySelector(".container").style.transition = ".4s all";
-    document.querySelector(".container").style.boxShadow = "0px 0px 10px #6dd1ff"
+    document.querySelector(".container").style.boxShadow = "0px 0px 10px #6dd1ff";
     intervalo = setInterval(() => {
         segundos++;
         if (segundos == 60) {
@@ -15,30 +16,40 @@ function iniciar() {
             minutos++;
         }
 
-        if(minutos == 60) {
+        if (minutos == 60) {
             minutos = 0;
             horas++;
         }
-
 
         document.querySelector(".texto").innerHTML = horas + " horas : " + minutos + " minutos : " + segundos + " segundos";
     }, 1000);
 }
 
 function pausar() {
-    clearInterval(intervalo);
-    document.querySelector(".container").style.boxShadow = "0px 0px 10px #ff0"
-    
+    if (horas == 0 && minutos == 0 && segundos == 0) {
+
+    } else {
+        clearInterval(intervalo);
+        document.querySelector(".container").style.boxShadow = "0px 0px 10px #ff0"
+    }
+
+    document.querySelector(".iniciar").setAttribute("onclick", "iniciar()");
 }
 
 function parar() {
-    clearInterval(intervalo);
-    segundos = 0;
-    minutos = 0;
-    horas = 0;
-    document.querySelector(".texto").innerHTML = horas + " horas : " + minutos + " minutos : " + segundos + " segundos";
-    document.querySelector(".container").style.boxShadow = "0px 0px 10px #f00"
-    setTimeout(() => {
-        document.querySelector(".container").style.boxShadow = "0px 0px 10px #fff";
-    }, 500);
+    if (horas == 0 && minutos == 0 && segundos == 0) {
+
+    } else {
+        clearInterval(intervalo);
+        segundos = 0;
+        minutos = 0;
+        horas = 0;
+        document.querySelector(".texto").innerHTML = horas + " horas : " + minutos + " minutos : " + segundos + " segundos";
+        document.querySelector(".container").style.boxShadow = "0px 0px 10px #f00"
+        setTimeout(() => {
+            document.querySelector(".container").style.boxShadow = "0px 0px 10px #fff";
+        }, 500);
+    }
+
+    document.querySelector(".iniciar").setAttribute("onclick", "iniciar()");
 }
